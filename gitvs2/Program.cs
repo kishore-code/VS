60597c0710fb95554ad8ad3fce8b5175f61c5198
Sai Kishore <kishore.zoho@gmail.com>
2020-04-14 04:53:46 +0530

    Initial check-in of module gitvs2

---
+++ gitvs2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gitvs2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from vs!");
+        }
+    }
+}
